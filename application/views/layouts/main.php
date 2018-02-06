556940e1f8fcf803330d9a8eddf1cd7034e1adff
adirahman <kodingku@github.com>
2018-02-06 14:00:29 +0700

    saya edit main view

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Main View</title>
+<title>main view ci app</title>
   <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
